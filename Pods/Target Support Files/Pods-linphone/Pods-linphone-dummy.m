202ffe0af3e3145657efa6ee6f7a34c8e8d612fb
Lyubomir Marinov <lyubomir.marinov@tryzens.com>
2020-08-20 21:38:42 +0300

    Remove Pods

--- Pods/Target Support Files/Pods-linphone/Pods-linphone-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_linphone : NSObject
-@end
-@implementation PodsDummy_Pods_linphone
-@end
--- Pods/Target Support Files/Pods-msgNotificationService/Pods-msgNotificationService-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_msgNotificationService : NSObject
-@end
-@implementation PodsDummy_Pods_msgNotificationService
-@end
